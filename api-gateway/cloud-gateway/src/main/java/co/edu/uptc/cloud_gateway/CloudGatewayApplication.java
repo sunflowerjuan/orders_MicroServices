c833f049f205b82a00a15337ab71aef65c697bd4
suflowerjuan <sebasjuan331@gmail.com>
2025-09-29 01:00:03 -0400

    api fixes

--- api-gateway/cloud-gateway/src/main/java/co/edu/uptc/cloud_gateway/CloudGatewayApplication.java
+++ api-gateway/cloud-gateway/src/main/java/co/edu/uptc/cloud_gateway/CloudGatewayApplication.java
@@ -2,8 +2,10 @@ package co.edu.uptc.cloud_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
