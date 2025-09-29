be8443ab8095a1662133423ea207bfb8f09165a0
suflowerjuan <sebasjuan331@gmail.com>
2025-09-29 00:51:57 -0400

    Api-gateway springboot initialization

---
+++ api-gateway/cloud-gateway/src/test/java/co/edu/uptc/cloud_gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.cloud_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
